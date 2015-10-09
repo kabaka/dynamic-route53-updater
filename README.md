e7062366e8e4c75e9573f83c52ca863849545b27
Kyle Johnson <kyle@vacantminded.com>
2015-10-09 03:40:35 +0000

    use command line args for configuration

--- README.md
+++ README.md
@@ -2,10 +2,27 @@
 
 Super simple script to update a hosted zone with the IP found using ipify.
 
-The hosted zone ID and record set name are hard-coded, but easy to update.
-
 I'm using this in places where IPs change often. It works "well enough" for me.
 
-_Note that this currently submits an UPSERT whether or not a change is needed.
-If I get some spare time, I'll have it poll (via regular DNS query) before
-doing this._
+## Installation
+
+At the moment, this is not available via npm.
+
+## Usage
+
+```
+    Options:
+
+    -h, --help                     output usage information
+    -V, --version                  output the version number
+    -z, --hosted-zone-id <zoneId>  Route53 Hosted Zone ID
+    -n, --name <name>              Resource Record Set Name
+    -t, --type <type>              Resource Record Set Type (default: A)
+    -T, --ttl <seconds>            Record Time-To-Live (default: 300)
+```
+
+## Notes
+
+This currently submits an UPSERT whether or not a change is needed.  If I get
+some spare time (or a pull request), I'll have it poll (via regular DNS query)
+before doing this.
--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 var async = require('async');
 var ipify = require('ipify');
+var opts  = require('commander');
 var AWS   = require('aws-sdk'),
   route53 = new AWS.Route53({ apiVersion: '2013-04-01' });
 
-var HostedZoneId = ''; /* XXX SET THIS */
-
-function getIpAddress(next) {
-  ipify(next);
+function getIpAddress(opts, next) {
+  ipify(function(err, ip, data) {
+    next(err, opts, ip, data);
+  });
 }
 
-function ipAddressReceived(ipAddress, data, next) {
+function ipAddressReceived(opts, ipAddress, data, next) {
   var params = {
-    HostedZoneId: HostedZoneId,
+    HostedZoneId: opts.hostedZoneId,
     ChangeBatch: {
       Changes: [
         {
           Action: 'UPSERT',
           ResourceRecordSet: {
-            Name: '', /* XXX AND THIS and whatever else in here is wanted */
-            Type: 'A',
+            Name: opts.name,
+            Type: opts.type || 'A',
             ResourceRecords: [
               {
                 Value: ipAddress
               }
             ],
-            TTL: 60
+            TTL: opts.ttl || 300
           }
         }
       ]
     }
   };
 
+  console.log(params);
+
   route53.changeResourceRecordSets(params, next);
 }
 
+opts
+  .version('1.0.0')
+  .option('-z, --hosted-zone-id <zoneId>', 'Route53 Hosted Zone ID')
+  .option('-n, --name <name>', 'Resource Record Set Name')
+  .option('-t, --type <type>', 'Resource Record Set Type (default: A)')
+  .option('-T, --ttl <seconds>', 'Record Time-To-Live (default: 300)')
+  .parse(process.argv);
+
 async.waterfall(
   [
+    function(next) {
+      next(null, opts);
+    },
     getIpAddress,
     ipAddressReceived
   ],
